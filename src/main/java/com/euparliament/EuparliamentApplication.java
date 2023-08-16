fa295bb1ec9ce8c958cf2071a6f7017c3aacc942
simonescaccia <72872543+simonescaccia@users.noreply.github.com>
2023-08-16 17:20:10 +0200

    Creating project

---
+++ src/main/java/com/euparliament/EuparliamentApplication.java
@@ -0,0 +1,13 @@
+package com.euparliament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuparliamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuparliamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/euparliament/EuparliamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euparliament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuparliamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
