13ddd5893e23bc872362dc5822ea34cd5dde05f9
Chiara Maggi <99491751+Chiaggi@users.noreply.github.com>
2023-08-21 16:44:48 +0200

    Update EuparliamentController.java

--- web/src/main/java/com/euparliament/web/EuparliamentController.java
+++ web/src/main/java/com/euparliament/web/EuparliamentController.java
@@ -18,11 +18,157 @@ public class EuparliamentController {
 		return "index";
 	}
 	
-	@GetMapping("/login")
-	public String login(
+	@GetMapping("/citizen")
+	public String citizen_index(
 			@RequestParam(name="name", required=false, defaultValue="") String name,
 			Model model
 	){
-		return "login";
+		model.addAttribute("name", name);
+		return "citizen_web/index";
 	}
+	
+	@GetMapping("/citizen/login")
+	public String citizen_login(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/login";
+	}
+	
+	@GetMapping("/citizen/registration")
+	public String citizen_registration(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/registration";
+	}
+	
+	@GetMapping("/citizen/home")
+	public String citizen_home(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/home";
+	}
+	
+	@GetMapping("/citizen/referendum")
+	public String citizen_referendum(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/referendum";
+	}
+	
+	@GetMapping("/citizen/ideas")
+	public String citizen_referendum_ideas(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/referendum_ideas";
+	}
+	
+	@GetMapping("/citizen/propose")
+	public String citizen_propose(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/propose_idea";
+	}
+	
+	@GetMapping("/citizen/idea_results")
+	public String citizen_idea_results(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/referendum_idea_results";
+	}
+	
+	@GetMapping("/citizen/results")
+	public String citizen_referendum_results(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/referendum_results";
+	}
+	
+	@GetMapping("/citizen/vote")
+	public String citizen_referendum_vote(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "citizen_web/referendum_vote";
+	}
+	
+	@GetMapping("/inst/login")
+	public String inst_login(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/login";
+	}
+	
+	@GetMapping("/inst/home")
+	public String inst_home(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/home";
+	}
+	
+	@GetMapping("/inst/referendum")
+	public String inst_referendum(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/referendum";
+	}
+	
+	@GetMapping("/inst/ideas")
+	public String inst_referendum_ideas(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/referendum_ideas";
+	}
+	
+	@GetMapping("/inst/propose")
+	public String inst_propose(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/propose_referendum";
+	}
+	
+	@GetMapping("/inst/idea_results")
+	public String inst_idea_results(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/referendum_idea_results";
+	}
+	
+	@GetMapping("/inst/results")
+	public String inst_referendum_results(
+			@RequestParam(name="name", required=false, defaultValue="") String name,
+			Model model
+	){
+		model.addAttribute("name", name);
+		return "inst_web/referendum_results";
+	}
+	
 }
