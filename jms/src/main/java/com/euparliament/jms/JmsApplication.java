ee2c99a82ea933a3ee27586ae9fa78ed0929e913
Riccardo Gobbato <100858226+RicGobs@users.noreply.github.com>
2023-08-22 11:25:41 +0200

    Add files via upload

---
+++ jms/src/main/java/com/euparliament/jms/JmsApplication.java
@@ -0,0 +1,13 @@
+package com.euparliament.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsApplication.class, args);
+	}
+
+}
