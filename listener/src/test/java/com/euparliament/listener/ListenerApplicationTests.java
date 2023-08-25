bc862752eb27aeb34754dcaa42cbd7a3323851ab
Riccardo Gobbato <100858226+RicGobs@users.noreply.github.com>
2023-08-25 17:36:17 +0200

    Add Listener MultiRabbitMQ (not working)

---
+++ listener/src/test/java/com/euparliament/listener/ListenerApplicationTests.java
@@ -0,0 +1,33 @@
+package com.euparliament.listener;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerAnnotationBeanPostProcessor;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.SimpleRoutingConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    @Autowired
+    private RabbitListenerAnnotationBeanPostProcessor rabbitListenerAnnotationBeanPostProcessor;
+
+    @Test
+    void shouldLoadConnectionFactoryBean() {
+        assertNotNull(connectionFactory);
+        assertTrue(connectionFactory instanceof SimpleRoutingConnectionFactory);
+    }
+
+    @Test
+    void shouldLoadRabbitListenerAnnotationBeanPostProcessorBean() {
+        assertNotNull(rabbitListenerAnnotationBeanPostProcessor);
+    }
+}
