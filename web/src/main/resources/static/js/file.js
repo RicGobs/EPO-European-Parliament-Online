f67f60d3b06a819fdd59463681bfcd24f3c34292
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-08-28 19:44:46 +0200

    Create file.js

---
+++ web/src/main/resources/static/js/file.js
@@ -0,0 +1,35 @@
+function redirect(loc) {
+  location.href = loc;
+  return;
+}
+
+// index.html
+function selection() {
+	//var selectedCountry = document.getElementById('country').value;
+	var selectedRole = document.getElementById('role').value;
+	console.log(selectedRole);
+	var redirect_path = '/'
+
+	
+	if (selectedRole == 'citizen') {
+		redirect_path = 'citizen';
+	}
+	else {
+		redirect_path = 'inst';
+	}
+
+	redirect(redirect_path);
+}
+
+
+citizen_registration() {
+
+}
+
+citizen_login() {
+
+}
+
+inst_login() {
+	
+}
