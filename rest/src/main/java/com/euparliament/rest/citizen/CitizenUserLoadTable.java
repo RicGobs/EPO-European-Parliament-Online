03e629437392dacb75eb784ca7430e65f0ecf04f
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-09-19 09:58:11 +0200

    Create CitizenUserLoadTable.java

---
+++ rest/src/main/java/com/euparliament/rest/citizen/CitizenUserLoadTable.java
@@ -0,0 +1,20 @@
+package com.euparliament.rest.citizen;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+  @Bean
+  CommandLineRunner initDatabase(CitizenUserRepository repository) {
+
+    return args -> {
+    };
+  }
+}
