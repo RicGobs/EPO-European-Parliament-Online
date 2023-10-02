f52eca7a9b97b106e54e1c979b3ff403ab796911
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-02 10:31:58 +0200

    Update CitizenUserRepository.java

--- rest/src/main/java/com/euparliament/rest/citizen/CitizenUserRepository.java
+++ rest/src/main/java/com/euparliament/rest/citizen/CitizenUserRepository.java
@@ -2,4 +2,10 @@ package com.euparliament.rest.citizen;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface CitizenUserRepository extends JpaRepository<CitizenUser, String> {}
+interface CitizenUserRepository extends JpaRepository<CitizenUser, Long> {
+
+  CitizenUser findByNationalID(String nationalID);
+
+	boolean existsByNationalID(String nationalID);
+  
+}
