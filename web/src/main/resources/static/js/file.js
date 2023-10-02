adcc275cf8cae35314d9fc7e22e9006369c80da0
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-02 09:34:08 +0200

    Update file.js

--- web/src/main/resources/static/js/file.js
+++ web/src/main/resources/static/js/file.js
@@ -1,4 +1,15 @@
-// index.html
+var country = "IT" //default
+
+var REST_ITA_URL = 'http://localhost:8081/';
+var REST_FRA_URL = 'http://localhost:8085/';
+var REST_GER_URL = 'http://localhost:8089/';
+
+var BROADCAST_ITA_URL = 'http://localhost:8082/';
+var BROADCAST_FRA_URL = 'http://localhost:8086/';
+var BROADCAST_GER_URL = 'http://localhost:8090/';
+
+
+//index.html
 function selection() {
 	var selectedCountry = document.getElementById('country').value;
 	var selectedRole = document.getElementById('role').value;
@@ -6,6 +17,7 @@ function selection() {
 	var redirect_path = '/'
 
 	if (selectedCountry == "IT" || selectedCountry == "FR" || selectedCountry == "DE") {
+		country = selectedCountry;
 		if (selectedRole == 'citizen') {
 			redirect_path = 'citizen';
 		}
@@ -14,28 +26,81 @@ function selection() {
 		}	
 	}
 
-	 location.href = redirect_path;
+	location.href = redirect_path;
 }
 
-function get_referendum_ideas() {
-	fetch("http://localhost:8081/referendumideaproposal", {
-	  method: 'GET'})
-    .then((response) => {
-      if (!response.ok) {
-      	throw new Error(`HTTP error: ${response.status}`);
-    }
-    	return response.text();
-    })
-    .then((text) => {	
-    	var array = JSON.parse(text);
-    	for (let i = 0; i < array.length; i++) {
-		  console.log(array[i].title);
+function submit_referendum() {
+	let confirmAction = confirm("Are you sure to propose this referendum?");
+	if (confirmAction) {
+
+		var title = document.getElementById('title').value;
+		var argument = document.getElementById('argument').value;
+
+		var URL = "";
+		if (country == "IT") URL = BROADCAST_ITA_URL.concat('europeanReferendumProposal');
+		if (country == "FR") URL = BROADCAST_FRA_URL.concat('europeanReferendumProposal');
+		if (country == "DE") URL = BROADCAST_GER_URL.concat('europeanReferendumProposal');
+
+		fetch(URL), {
+			method: 'POST',
+			headers: {
+			  'Content-type': 'application/json',
+			  'Access-Control-Allow-Origin': '*'
+			},
+			body: JSON.stringify({
+			  'title': title,
+			  'argument': argument
+			})
+		};
+		  
+		alert("Referendum proposed.");
+		location.href = 'inst/home';
+	 } 
+	 else {
+		alert("Proposal canceled.");
+	}
+  }
+
+  /*
+  function vote_referendum() {
+
+	var title = document.getElementById('title');
+	var dateStartConsensusProposal = document.getElementById('start-date');
+
+	var URL = "";
+
+	var elems = document.getElementsByTagName('input');
+	for (i = 0; i < elems.length; i++) {
+        if (elems[i].type = "radio" && elems[i].checked) {
+			if (elems[i].value == "Yes") {
+				if (country == "IT") URL = REST_ITA_URL.concat('voteTrue');
+				if (country == "FR") URL = REST_FRA_URL.concat('voteTrue');
+				if (country == "DE") URL = REST_GER_URL.concat('voteTrue');
+			}
+			else {
+				if (country == "IT") URL = REST_ITA_URL.concat('voteFalse');
+				if (country == "FR") URL = REST_FRA_URL.concat('voteFalse');
+				if (country == "DE") URL = REST_GER_URL.concat('voteFalse');
+			}
 		}
-  	});
-}
+	}
 
-async function get_referendum() {
-	await fetch("http://localhost:8081/referendums", {
+	fetch(URL.concat('?title=').concat(title).concat('&dateStartConsensusProposal=').concat(dateStartConsensusProposal));
+	alert('Thank you! Your vote has been correctly registered!');
+
+
+  }
+  */
+
+
+function get_referendum() {
+
+	var URL = "";
+	if (country == "IT") URL = REST_ITA_URL.concat('referendums');
+	if (country == "FR") URL = REST_FRA_URL.concat('referendums');
+	if (country == "DE") URL = REST_GER_URL.concat('referendums');
+
+	fetch(URL, {
 	  method: 'GET'})
     .then((response) => {
       if (!response.ok) {
@@ -75,8 +140,6 @@ async function get_referendum() {
 
 function citizen_registration() {
 
-	var ITA_DB_URL = 'http://localhost:8081/';
-
 	var national_id = document.getElementById('national_id').value;
 	
 	var name = document.getElementById('name').value;
@@ -92,9 +155,14 @@ function citizen_registration() {
 	var email = document.getElementById('email').value;
 	var cellular = document.getElementById('cellular').value;
 	var password = document.getElementById('password').value;
+
+	var URL = "";
+	if (country == "IT") URL = REST_ITA_URL.concat('citizens');
+	if (country == "FR") URL = REST_FRA_URL.concat('citizens');
+	if (country == "DE") URL = REST_GER_URL.concat('citizens');
 	
 	
-	fetch(ITA_DB_URL.concat('citizens'), {
+	fetch(URL, {
 	  method: 'POST',
 	  headers: {
 	    'Content-type': 'application/json'
@@ -116,7 +184,7 @@ function citizen_registration() {
 	  })
 	});
 	
-	 location.href = 'citizen/home';
+	location.href = 'citizen/home';
 
 }	
 
@@ -124,8 +192,14 @@ function citizen_login() {
 	
 	var username = document.getElementById('username').value;
 	var password = document.getElementById('password').value;
+
+	var URL = "";
+	if (country == "IT") URL = REST_ITA_URL.concat('citizens');
+	if (country == "FR") URL = REST_FRA_URL.concat('citizens');
+	if (country == "DE") URL = REST_GER_URL.concat('citizens');
 	
-	fetch("http://localhost:8081/citizens", {
+	
+	fetch(URL, {
 	  method: 'GET'})
     .then((response) => {
       if (!response.ok) {
@@ -145,11 +219,32 @@ function citizen_login() {
 	// TO-DO: ASSIGN SESSION COOKIE 
 }
 
-/*
+
 function inst_login() {
 	
 	var username = document.getElementById('email').value;
 	var password = document.getElementById('password').value;
+
+	var URL = "";
+	if (country == "IT") URL = REST_ITA_URL.concat('representatives');
+	if (country == "FR") URL = REST_FRA_URL.concat('representatives');
+	if (country == "DE") URL = REST_GER_URL.concat('representatives');
+	
+	fetch(URL, {
+	  method: 'GET'})
+    .then((response) => {
+      if (!response.ok) {
+      	throw new Error(`HTTP error: ${response.status}`);
+    }
+    	return response.text();
+    })
+    .then((text) => {	
+    	var representatives = JSON.parse(text);
+    	for (let i = 0; i < representatives.length; i++) {
+		  if (username == representatives[i].email && password == representatives[i].password) {
+		  	 location.href = 'home';
+		  }
+	}
+  	});
 	
 }
-*/
