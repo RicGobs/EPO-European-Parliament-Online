f1ee18e76155f677a5966d5faf238d68b67cd804
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-04 11:00:56 +0200

    Update WebController.java

--- web/src/main/java/com/euparliament/web/WebController.java
+++ web/src/main/java/com/euparliament/web/WebController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.euparliament.web.utils.HttpRequest;
+import com.euparliament.web.exception.NotFoundException;
+import com.euparliament.web.model.CitizenUser;
+import com.euparliament.web.model.InstUser;
+import com.euparliament.web.model.Referendum;
+import com.euparliament.web.model.ResourceMapping;
 
 
 @Controller
 public class WebController {
+
+	@Autowired
+	ResourceMapping resourceMapping;
 	
 	@GetMapping("/")
 	public String index(
@@ -29,13 +40,31 @@ public class WebController {
 	
 	@GetMapping("/citizen/login")
 	public String citizen_login(
-			@RequestParam(name="name", required=false, defaultValue="") String name,
+			@RequestParam(name="uname", required=false, defaultValue="") String nationalID, 
+			@RequestParam(name="psw", required=false, defaultValue="") String password,
 			Model model
 	){
-		model.addAttribute("name", name);
-		return "citizen_web/login";
+		if (nationalID.equals("") && password.equals("")) {
+			return "citizen_web/login";
+		}
+		try {
+			// get citizen data structure from the database
+			CitizenUser citizen;
+			citizen = HttpRequest.getCitizenUser(
+					nationalID,
+					password,
+					resourceMapping
+			);
+			if (citizen == null) {
+				return "citizen_web/login";
+			}
+			return "citizen_web/home";
+		} catch (NotFoundException e) {
+			// citizen not found
+			return "citizen_web/login";
+		}
 	}
-	
+
 	@GetMapping("/citizen/registration")
 	public String citizen_registration(
 			@RequestParam(name="name", required=false, defaultValue="") String name,
@@ -74,20 +103,51 @@ public class WebController {
 	
 	@GetMapping("/citizen/vote")
 	public String citizen_referendum_vote(
-			@RequestParam(name="name", required=false, defaultValue="") String name,
+			@RequestParam(name="title", required=false, defaultValue="") String title,
+			@RequestParam(name="date", required=false, defaultValue="") String date,
 			Model model
 	){
-		model.addAttribute("name", name);
-		return "citizen_web/referendum_vote";
+		try {
+			// get referendum data structure from the database
+			Referendum referendum;
+			referendum = HttpRequest.getReferendum(
+					title,
+					date,
+					resourceMapping
+			);
+			model.addAttribute("title", title);
+			return "citizen_web/referendum_vote";
+		} catch (NotFoundException e) {
+			// representative not found
+			return "citizen_web/referendum";
+		}
 	}
-	
+
 	@GetMapping("/inst/login")
 	public String inst_login(
-			@RequestParam(name="name", required=false, defaultValue="") String name,
+			@RequestParam(name="uname", required=false, defaultValue="") String representativeID, 
+			@RequestParam(name="psw", required=false, defaultValue="") String password,
 			Model model
 	){
-		model.addAttribute("name", name);
-		return "inst_web/login";
+		if (representativeID.equals("") && password.equals("")) {
+			return "citizen_web/login";
+		}
+		try {
+			// get representative data structure from the database
+			InstUser representative;
+			representative = HttpRequest.getInstUser(
+					representativeID,
+					password,
+					resourceMapping
+			);
+			if (representative == null) {
+				return "inst_web/login";
+			}
+			return "inst_web/home";
+		} catch (NotFoundException e) {
+			// representative not found
+			return "inst_web/login";
+		}
 	}
 	
 	@GetMapping("/inst/home")
