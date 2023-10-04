ee763a76e1f4d81e05218863cb49e94b0981f6ce
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-04 11:05:27 +0200

    Create NotFoundException.java

---
+++ web/src/main/java/com/euparliament/web/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.euparliament.web.exception;
+
+public class NotFoundException extends Exception {
+
+	private static final long serialVersionUID = 927526875957075419L;
+	
+	public NotFoundException() {
+		super();
+	}
+	
+}
