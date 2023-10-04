58ec89ae31ccc75d983db0b819c7796ba1279f86
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-04 10:57:44 +0200

    Update InstUserRepository.java

--- rest/src/main/java/com/euparliament/rest/inst/InstUserRepository.java
+++ rest/src/main/java/com/euparliament/rest/inst/InstUserRepository.java
@@ -2,4 +2,9 @@ package com.euparliament.rest.inst;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface InstUserRepository extends JpaRepository<InstUser, String> {}
+interface InstUserRepository extends JpaRepository<InstUser, String> {
+
+  InstUser findByRepresentativeID(String representativeID);
+
+	boolean existsByRepresentativeID(String representativeID);
+}
