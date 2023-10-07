c9d6d49af488bae59ead1c5ddd0a62e86feff5fc
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-07 16:15:16 +0200

    Update InstUserController.java

--- rest/src/main/java/com/euparliament/rest/inst/InstUserController.java
+++ rest/src/main/java/com/euparliament/rest/inst/InstUserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin
 class InstUserController {
 
   private final InstUserRepository repository;
