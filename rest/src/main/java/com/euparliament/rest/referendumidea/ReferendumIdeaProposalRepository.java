af9e6b74f19d6e7a46a38e142b2c4316fd2fa227
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-07 16:16:03 +0200

    Delete rest/src/main/java/com/euparliament/rest/referendumidea directory

--- rest/src/main/java/com/euparliament/rest/referendumidea/ReferendumIdeaProposalRepository.java
+++
@@ -1,5 +0,0 @@
-package com.euparliament.rest.referendumidea;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface ReferendumIdeaProposalRepository extends JpaRepository<ReferendumIdeaProposal, Long> {}
