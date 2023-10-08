23ec005beb00eb63051bcbc00447d67fa758ab7c
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-10-08 11:50:50 +0200

    Update ResourceMapping.java

--- web/src/main/java/com/euparliament/web/model/ResourceMapping.java
+++ web/src/main/java/com/euparliament/web/model/ResourceMapping.java
@@ -16,34 +16,40 @@ public class ResourceMapping {
 	String representativesUrl;
 
     public ResourceMapping(
-    		@Value("${rest.url}") String restUrl
+    		@Value("${rest.url}") String restUrl,
+		@Value("${nation}") String nation
     ){
     	this.restTemplate = new RestTemplate();
     	this.restUrl = restUrl;
-		this.citizenUrlLogin = restUrl + "/citizenLogin";
-		this.representativeUrlLogin = restUrl + "/representativeLogin";
-		this.referendumUrl = restUrl + "/referendum";
-		this.citizensUrl = restUrl + "/citizens";
-		this.representativesUrl = restUrl + "/representatives";
+	this.nation = nation;
+	this.citizenUrlLogin = restUrl + "/citizenLogin";
+	this.representativeUrlLogin = restUrl + "/representativeLogin";
+	this.referendumUrl = restUrl + "/referendum";
+	this.citizensUrl = restUrl + "/citizens";
+	this.representativesUrl = restUrl + "/representatives";
 
     }
-    
+
+	public String getNation() {
+		return this.nation;
+	}
+	
 	public String getCitizenUrlLogin() {
 		return this.citizenUrlLogin;
 	}
-
+	
 	public String getRepresentativeUrlLogin() {
 		return this.representativeUrlLogin;
 	}
-
+	
 	public String getReferendumUrl() {
 		return this.referendumUrl;
 	}
-
+	
 	public String getCitizensUrl() {
 		return this.citizensUrl;
 	}
-
+	
 	public String getRepresentativesUrl() {
 		return this.representativesUrl;
 	}
